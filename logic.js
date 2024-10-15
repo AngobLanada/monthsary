d52fafbbac845cac6d99ca8104839f3b602553d9
JUDE LANADA <151989044+AngobLanada@users.noreply.github.com>
2024-10-16 02:33:53 +0800

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,36 @@
+function checkPassword() {
+    var name =  document.getElementById("name").value;
+    var password =  document.getElementById("password").value;
+    if (name.toLowerCase() == "july" && password.toLowerCase() == "16") 
+        window.open("video.html", '_blank');
+    else {
+        document.getElementById("form-container").classList.add("form-input-animation");
+        setTimeout(() => {
+            document.getElementById("form-container").classList.remove("form-input-animation");
+        }, 1000);
+    }
+    document.getElementById("name").value = "";
+    password =  document.getElementById("password").value = "";
+}
+
+low_opacity = "brightness(20%)";
+max_opacity = "brightness(100%)";
+
+function disapeare_image(imageID, textID) {
+    document.getElementById(textID).style.visibility = "visible";
+    document.getElementById(imageID).style.filter = low_opacity;
+}
+
+function apeare_image(imageID, textID) {   
+    setTimeout(() => {
+        document.getElementById(imageID).style.filter = max_opacity;
+        document.getElementById(textID).style.visibility = "hidden";
+    }, 50);
+    
+}
+
+function openSourceCode() {window.open("https://github.com/Nikolay1580/Anniversarry-Website"), "_blank";}
+
+function goToYear2() {
+    window.location.href = "year2.html"
+}
